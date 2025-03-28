3880995b0ba8b338e03e889a7731bdf5c158ac20
Matthieu Fallourd <fallourd.matthieu@gmail.com>
2025-03-28 16:42:37 +0100

    feat: initialisation

---
+++ projects/icons/src/lib/icons.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-icons',
+  imports: [],
+  template: `
+    <p>
+      icons works!
+    </p>
+  `,
+  styles: ``
+})
+export class IconsComponent {
+
+}
